83c90a77d9390ce730e5bb28cd7f6da6df179d9f
Alex <Suarezalex596@gmail.com>
2021-08-13 22:17:11 -0500

    Comienzo del taller

---
+++ Principal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taller_POO1
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
